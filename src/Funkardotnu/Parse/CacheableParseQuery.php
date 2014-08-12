a5b02328c6e9726f2e7bd424f5ce211c16bc37c9
Henrik Malmberg <henrik.malmberg@kaigan.se>
2014-08-12 22:03:15 +0200

    seriously, typo's everywhere..

--- src/Funkardotnu/Parse/CacheableParseQuery.php
+++ src/Funkardotnu/Parse/CacheableParseQuery.php
@@ -8,7 +8,7 @@ use \Doctrine\Common\Cache\CacheProvider;
 /**
  * CacheableParseQuery - Handles querying data from Parse - with cache!
  *
- * @package Funkardornu\Parse
+ * @package Funkardotnu\Parse
  * @author Henrik Malmberg <henrik@funkar.nu>
  */
 class CacheableParseQuery extends ParseQuery {
